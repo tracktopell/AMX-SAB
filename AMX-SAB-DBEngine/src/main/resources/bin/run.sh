a20fa22777916090d32cffa8312c14b658e9746c
alfredo.estrada <alfredo.estrada@userpc-HP>
2017-10-04 17:11:14 -0500

    Adding files for DErby srat/stop souport

--- AMX-SAB-DBEngine/src/main/resources/bin/run.sh
+++ AMX-SAB-DBEngine/src/main/resources/bin/run.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 java -Dderby.system.home=$HOME${nix.file.separator}DERBY_DATABASES  -cp dependency-jars${nix.file.separator}derby.jar${nix.path.separator}dependency-jars${nix.file.separator}derbyLocale_es.jar${nix.path.separator}dependency-jars${nix.file.separator}derbyclient.jar${nix.path.separator}dependency-jars${nix.file.separator}derbynet.jar${nix.path.separator}${project.artifactId}.jar ${project.mainclass} start -h localhost -p  ${derby.server.port}  
+for i in `ps -fea|awk '{if(index($0,"derby")&&!index($0,"grep")){printf("%d",$2);}}'`; do
+  echo "#!/bin/bash" >  ./stopCurrentDerby.sh;
+  echo "kill -9 $i"  >> ./stopCurrentDerby.sh;
+done;
