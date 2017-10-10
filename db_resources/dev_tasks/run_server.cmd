3aaf15056bbabde8fac4ab1ce2ab8a4b692bc513
alfredo.estrada <alfredo.estrada@userpc-HP>
2017-10-10 07:26:03 -0500

    fixing files in db_resources and scripts

--- db_resources/dev_tasks/run_server.cmd
+++
@@ -1,1 +0,0 @@
-java -Dderby.system.home=%USER_HOME%\DERBY_DATABASES  -cp dependency-jars\derby.jar;dependency-jars\derbyLocale_es.jar;dependency-jars\derbyclient.jar;dependency-jars\derbynet.jar;AMX-SAB-DBEngine.jar com.aeromexico.sab.dbengine.DerbyServerLauncherWrapper start -h localhost -p 1527 
--- db_resources/dev_tasks/run_server.sh
+++
@@ -1,1 +0,0 @@
-﻿java -Dderby.system.home=$HOME/DERBY_DATABASES  -cp dependency-jars/derby.jar:dependency-jars/derbyLocale_es.jar:dependency-jars/derbyclient.jar:dependency-jars/derbynet.jar:AMX-SAB-DBEngine.jar com.aeromexico.sab.dbengine.DerbyServerLauncherWrapper start -h localhost -p  1527  
